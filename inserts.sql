d6783f748da8b14b4558810514efcdc74dddc779
Oscar Escobar <qomc16@gmail.com>
2022-11-17 17:18:57 -0600

    Cambios en la base de datos

--- inserts.sql
+++ inserts.sql
@@ -6,3 +6,6 @@ insert into rol_tipo_usuario (id_rol,id_tipo_usuario) values(6,16);
 insert into cliente (idpersona,id_tipo_usuario,fecha_alta,usuario_cliente,contrasena) values(1,16,'02/05/2022','Juan','remember123');
 insert into correo (idpersona,idcliente,email) values(1,1,'correogenerico@gmail.com');
 insert into telefono (idpersona,idcliente,numero) values (1,1,'52304059');
+insert into empleado (id_tipo_usuario,idpersona,fecha_ingreso,fecha_egreso,usuario,contrasena) values(1,1,'02/04/2000','17/11/2022','juancho','contrasena');
+insert into tipo_documento (nacionalidad,pasaporte,licencia) values ('Guatemalteca','Pasaporte Ordinario','A');
+insert into documento_persona (id_tipopersona,idPersona) values (2,1);
